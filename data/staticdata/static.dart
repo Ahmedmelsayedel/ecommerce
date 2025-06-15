8b4b107e70d24195cedfe2fbbe9c9b106f1e5654
Ahmedmelsayedel <am20149687@gmail.com>
2025-06-15 18:59:51 +0300

    Add files via upload

---
+++ data/staticdata/static.dart
@@ -0,0 +1,11 @@
+import 'package:ecommerce/core/constant/imageasset.dart';
+import 'package:ecommerce/data/model/onbordingmodel.dart';
+import 'package:get/get.dart';
+
+List<Onbordingmodel> onbordingmodel = [
+  Onbordingmodel("${Imageasset.imageone}", "3".tr, "4".tr),
+  Onbordingmodel("${Imageasset.imagethree}","5".tr,"6".tr
+      ),
+  Onbordingmodel("${Imageasset.imagetow}","7".tr,"8".tr
+     )
+];
