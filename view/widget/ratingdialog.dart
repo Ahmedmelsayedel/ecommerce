66a7b416420ae6b493e9d13f2ef879e20f59a4cc
Ahmedmelsayedel <am20149687@gmail.com>
2025-06-15 18:33:07 +0300

    Add files via upload

---
+++ view/widget/ratingdialog.dart
@@ -0,0 +1,41 @@
+import 'package:ecommerce/controller/orderarchivecontrller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:rating_dialog/rating_dialog.dart';
+
+// show the dialog
+void showdialog(BuildContext context) {
+  Orderarchivecontrller controller = Get.find();
+  showDialog(
+      context: context,
+      barrierDismissible: true, // set to false if you want to force a rating
+      builder: (context) => RatingDialog(
+        enableComment: false,
+          initialRating: 1.0,
+          // your app's name?
+          title: Text(
+            'Rating Dialog',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // encourage your user to leave a high rating?
+          message: Text(
+            'Tap a star to set your rating. Add more description here if you want.',
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 15),
+          ),
+
+          // your app's logo?
+          //  image: Image.asset(Imageasset.imagethree,height: 15,),
+          submitButtonText: 'Submit',
+          commentHint: 'Set your custom comment hint',
+          onCancelled: () => print('cancelled'),
+          onSubmitted: (response) {
+            controller.updatrate(response.rating, response.comment);
+            print('rating: ${response.rating}, comment: ${response.comment}');
+          }));
+}
